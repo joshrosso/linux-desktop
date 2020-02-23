c94d3480cd43a6cc5ceccc0d0fb96eeb62e02c2a
joshrosso <joshrosso@gmail.com>
2020-02-23 13:46:52 -0700

    Automate grub fonts

--- pkg/configure-system.sh
+++ pkg/configure-system.sh
@@ -20,8 +20,6 @@ mkinitcpio -p linux
 # map vim to nvim
 ln -sf /usr/bin/nvim /usr/bin/vim
 
-# need to redo grub-mkconfig so it includes ucode launch before initramfs
-grub-mkconfig -o /boot/grub/grub.cfg
 
 localectl set-locale LANG=en_US.UTF-8
 
@@ -31,4 +29,12 @@ cp -r ../scripts/usr/local/bin/importc /usr/local/bin/
 # make vconsole font larger
 VCONSOLE_FONT=FONT=ter-v32n.psf.gz
 touch /etc/vconsole.conf
-grep -qxF ${VCONSOLE_FONT} /etc/vconsole.conf || echo ${VCONSOLE_FONT} >> /etc/vconsole.conf
+grep -qxF ${VCONSOLE_FONT} /etc/vconsole.conf || sudo echo ${VCONSOLE_FONT} >> /etc/vconsole.conf
+
+# set grub font size
+grub-mkfont -s 60 -o /boot/grubfont.pf2 /usr/share/fonts/TTF/Hack-Regular.ttf
+GRUB_FONT=GRUB_FONT=\"/boot/grubfont.pf2\"
+grep -qxF ${GRUB_FONT} /etc/default/grub || echo ${GRUB_FONT} >> /etc/default/grub
+
+# update grub config
+grub-mkconfig -o /boot/grub/grub.cfg

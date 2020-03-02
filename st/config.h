043281225fdca128ed456164d2609bd60d7bbf7f
joshrosso <joshrosso@gmail.com>
2020-03-02 10:12:01 -0700

    add pasystray and remove transparency from terminal

--- st/config.h
+++ st/config.h
@@ -84,7 +84,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.92;
+float alpha = 1.00;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
